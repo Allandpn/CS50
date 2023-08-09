f21852a69907dfe1d74a232122a4f31fa8ce4073
Allandpn <99135710+Allandpn@users.noreply.github.com>
2023-08-08 22:31:14 -0300

    flask

--- CS50-2023/SQL/Set Problem/fiftyville/a.sql
+++ CS50-2023/SQL/Set Problem/fiftyville/a.sql
@@ -16,8 +16,8 @@ select name from people where passport_number in (
             and month = 07 
             and year = 2021 
             and duration < 60)
-        and name in (
-            select name from people 
+        and people.id in (
+            select people.id from people 
             where license_plate in(
                 select license_plate from bakery_security_logs 
                 where activity = 'exit' 
@@ -25,9 +25,9 @@ select name from people where passport_number in (
                 and month = 07 
                 and year = 2021 
                 and hour = 10 
-                and minute between 15 and 25))        
-        and name in (
-            select name from people as pp 
+                and minute between 10 and 30))        
+        and people.id in (
+            select people.id from people as pp 
             join bank_accounts as ba on pp.id = ba.person_id
             join atm_transactions as at on ba.account_number = at.account_number
             where at.day = 28
